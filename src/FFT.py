2a046e79a8b51b2df396bf3240d83108d3f20fe3
Benno Weber <bennopaul.weber@gmail.com>
2025-08-18 21:27:29 +0200

    FFT does not give right values

--- src/FFT.py
+++ src/FFT.py
@@ -10,11 +10,11 @@ fs, data = wavfile.read(AUDIO_FILE)  #Return the sample rate (in samples/sec) an
 audio = data.T[0]       # 1st channel of wav
 
 
+FFT_WINDOW_SIZE = 2048
+FFT_WINDOW_SECONDS = FFT_WINDOW_SIZE/fs # how many seconds of audio make up an FFT window
 
-FFT_WINDOW_SECONDS = 0.046 # how many seconds of audio make up an FFT window
 
-
-FFT_WINDOW_SIZE = int(fs * FFT_WINDOW_SECONDS)
+#FFT_WINDOW_SIZE = int(fs * FFT_WINDOW_SECONDS)
 AUDIO_LENGTH = len(audio)/fs
 
 NOTE_NAMES = ["do", "do#", "ré", "ré#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si"]
@@ -41,6 +41,7 @@ def round_note_num(num): return int(round(num,0))%12
 window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, FFT_WINDOW_SIZE, False)))
 
 print(len(audio))
+print(FFT_WINDOW_SECONDS)
 for tick in range(int(len(audio)/FFT_WINDOW_SIZE)):
     sample = extract_sample(audio, tick)
     fft = rfft(sample * window)
@@ -48,7 +49,7 @@ for tick in range(int(len(audio)/FFT_WINDOW_SIZE)):
         if 1000 >= freq.real >= 10:
             note_num = freq_to_number(freq.real)
             if (note_num % 1) < 0.005 or (note_num % 1) > 0.995:
-                if abs(freq.imag) > 5000:
+                if abs(freq.imag) > 1000:
                     note_num = round_note_num(note_num)
                     print("tick:",(tick*FFT_WINDOW_SIZE) ,"second:",round((tick*FFT_WINDOW_SIZE/fs),4) ,NOTE_NAMES[note_num],"frequency:",freq.real,"intensity:", round(freq.imag,0))
 
