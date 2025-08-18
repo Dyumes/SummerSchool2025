8a822c54f722d6dc72b5467b480faa9c9998be38
Benno Weber <bennopaul.weber@gmail.com>
2025-08-18 16:10:11 +0200

    started FFT

--- src/FFT.py
+++ src/FFT.py
@@ -1,2 +1,12 @@
-import scipy
+from scipy.fftpack import fft
+from scipy.io import wavfile
+import os
+
+
 print("tonsebastian")
+
+AUDIO_FILE = os.path.join("media","wav","test.wav")
+
+fs, data = wavfile.read(AUDIO_FILE)  #Return the sample rate (in samples/sec) and data from an LPCM WAV file
+audio = data.T[0]       # 1st channel of wav
+
