a2c18b2391ab198bb4ad156907faeaacc1e614fa
Benno Weber <bennopaul.weber@gmail.com>
2025-08-19 10:09:41 +0200

    FFT -> no filter

--- src/FFT.py
+++ src/FFT.py
@@ -1,16 +1,16 @@
-from scipy.fft import rfft
+from scipy.fft import rfft, rfftfreq
 from scipy.io import wavfile
 import os
 import numpy as np
 
 
-AUDIO_FILE = os.path.join("media","wav","test.wav")
+AUDIO_FILE = os.path.join("media","wav","PinkPanther_Piano_Only.wav")
 
 fs, data = wavfile.read(AUDIO_FILE)  #Return the sample rate (in samples/sec) and data from an LPCM WAV file
 audio = data.T[0]       # 1st channel of wav
 
 
-FFT_WINDOW_SIZE = 2048
+FFT_WINDOW_SIZE = 4096
 FFT_WINDOW_SECONDS = FFT_WINDOW_SIZE/fs # how many seconds of audio make up an FFT window
 
 
@@ -42,16 +42,39 @@ window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, FFT_WINDOW_SIZE, False)))
 
 print(len(audio))
 print(FFT_WINDOW_SECONDS)
+
 for tick in range(int(len(audio)/FFT_WINDOW_SIZE)):
     sample = extract_sample(audio, tick)
     fft = rfft(sample * window)
-    for freq in fft:
-        if 1000 >= freq.real >= 10:
-            note_num = freq_to_number(freq.real)
-            if (note_num % 1) < 0.005 or (note_num % 1) > 0.995:
-                if abs(freq.imag) > 1000:
-                    note_num = round_note_num(note_num)
-                    print("tick:",(tick*FFT_WINDOW_SIZE) ,"second:",round((tick*FFT_WINDOW_SIZE/fs),4) ,NOTE_NAMES[note_num],"frequency:",freq.real,"intensity:", round(freq.imag,0))
+
+    freqs = rfftfreq(FFT_WINDOW_SIZE,1/fs)
+
+    idx = 1
+    string =""
+    string += f"   second: {round((tick*FFT_WINDOW_SIZE/fs),4)}"
+    something = 0
+
+    while idx<len(freqs):
+        #print(freqs[idx],np.abs(fft[idx]))
+        if(np.abs(fft[idx]) > 700000) and ((freq_to_number(freqs[idx]) % 1) < 0.2 or (freq_to_number(freqs[idx]) % 1) > 0.8):
+            something = 1
+           # string += "  Note:"
+            string += "  "
+            string += NOTE_NAMES[round_note_num(freq_to_number(freqs[idx]))]
+            string += "  Velocity:"
+            string += str(round(np.abs(fft[idx]),0))
+            string += "  frequency:"
+            string += str(freqs[idx])
+
+        idx += 1
+    if something:
+        print(string)
+
+
+
+
+
+                    #print("tick:",(tick*FFT_WINDOW_SIZE) ,"second:",round((tick*FFT_WINDOW_SIZE/fs),4) ,NOTE_NAMES[note_num],"frequency:",freq.real,"intensity:", round(freq.imag,0))
 
 #print(round_note_num(freq_to_number(440)))
 #print(round_note_num(freq_to_number(16.35)))
