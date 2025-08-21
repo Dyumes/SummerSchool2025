a9b90347c28805584f579442798b3105c985c112
Benno Weber <bennopaul.weber@gmail.com>
2025-08-22 00:33:29 +0200

    better freq anal, added a contextual noise reduction

--- src/FFT.py
+++ src/FFT.py
@@ -12,7 +12,7 @@ AUDIO_FILE = os.path.join("media","wav","PinkPanther_Piano_Only.wav")
 fs, data = wavfile.read(AUDIO_FILE)  #Return the sample rate (in samples/sec) and data from an LPCM WAV file
 audio = data.T[0]       # 1st channel of wav
 
-FFT_WINDOW_SIZE = 2048          # 2048 -> 50ms, 21 Hrz //4096 -> 100ms, 10Hz
+FFT_WINDOW_SIZE = 2048         # 2048 -> 50ms, 21 Hrz //4096 -> 100ms, 10Hz
 FFT_WINDOW_SECONDS = FFT_WINDOW_SIZE/fs # how many seconds of audio make up an FFT window
 #FFT_WINDOW_SIZE = int(fs * FFT_WINDOW_SECONDS)
 AUDIO_LENGTH_TICKS = len(audio)
@@ -23,6 +23,9 @@ ZERO_PADDING_FACTOR = 2 #doubles precision(time or frequency)
 NOTE_NAMES = ["do", "do#", "ré", "ré#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si"]
 
 STEP_NUMBER = int(len(audio)/FFT_WINDOW_SIZE)
+
+BASE_TRESH = 250000
+ANY_TRESH = 50000
 
 def extract_sample(audio, step):    #exctrats window size sample from audio with zero-padding
     end = step*FFT_WINDOW_SIZE
@@ -43,18 +46,21 @@ def freq_to_number(freq): return (69 + 12*np.log2(freq/440.0))%12
 def round_note_num(num): return int(round(num,0))%12
 
 # Hanning window function
+
+sigma_factor = 8
 hanningWindow = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, FFT_WINDOW_SIZE, False)))
-gaussianWindow = scipy.signal.windows.gaussian(FFT_WINDOW_SIZE,FFT_WINDOW_SIZE/8,False)
+gaussianWindow = scipy.signal.windows.gaussian(FFT_WINDOW_SIZE,FFT_WINDOW_SIZE/sigma_factor,False)
 
 class Note :
     def __init__(self,freq,ampl):
         self.frequency = freq
         self.amplitude = ampl
         self.number = round_note_num(freq_to_number(self.frequency))        #number corresponding to note name
-        self.name = NOTE_NAMES[self.number]
 
     def __str__(self):
-        return f"|FEQ: {self.frequency} AMP: {self.amplitude} NOTE: {self.name}|"
+        return f"|FEQ: {self.frequency} AMP: {self.amplitude} NOTE: {self.name()}|"
+
+    def name(self): return NOTE_NAMES[self.number]
 
 class timeNotes :
     def __init__(self,step,notes):
@@ -118,14 +124,14 @@ def filter(values,interp):
     for timeNote in values:
         notes = []
 
-        i = 0
+        i = 2 # if i = 1 and peak then i-1 = 0 = freq 0 Hz which will cause lots of problems
         while i < len(timeNote.notes):
-            if timeNote.notes[i].amplitude > 100000:
+            if timeNote.notes[i].amplitude > ANY_TRESH :
                 while timeNote.notes[i].amplitude < timeNote.notes[i+1].amplitude:
                     i += 1
                 if timeNote.notes[i].amplitude > timeNote.notes[i-1].amplitude:
                     if interp == "gaussian":
-                        inter_max, inter_max_magnitude = interp_parabolic(timeNote.notes[i-1],timeNote.notes[i],timeNote.notes[i+1])
+                        inter_max, inter_max_magnitude = interp_gaussian(timeNote.notes[i-1],timeNote.notes[i],timeNote.notes[i+1])
                     else:
                         inter_max, inter_max_magnitude = interp_parabolic(timeNote.notes[i-1],timeNote.notes[i],timeNote.notes[i+1])
 
@@ -146,52 +152,12 @@ def printvalues(values):
 #printvalues(values)
 
 
-def getharmonics(notelist,name):
-    harmonicslist = []
+def getaudiblefreqs(notelist):
+    audiblefreqs = []
     for note in notelist:
-        if note.name == name:
-            harmonicslist.append(note)
-    return harmonicslist
-
-def pianovtrumpet(values):
-    trumpetvalues = []
-    pianovalues = []
-    allvalues = []
-
-    for timeNote in values:
-        trumpetnotes = []
-        pianonotes = []
-        allnotes = []
-
-        for name in NOTE_NAMES:
-            harmonicslist = getharmonics(timeNote.notes,name)
-            if len(harmonicslist)>1:
-                if harmonicslist[0].frequency < 1000 and harmonicslist[0].amplitude > 400000:
-
-                    if harmonicslist[0].amplitude *1.8 < harmonicslist[1].amplitude:
-                        trumpetnotes.append(harmonicslist[0])
-                    else:
-                        pianonotes.append(harmonicslist[0])
-                    allnotes.append(harmonicslist[0])
-
-        trumpetvalues.append(timeNotes(timeNote.step,trumpetnotes))
-        pianovalues.append(timeNotes(timeNote.step,pianonotes))
-        allvalues.append(timeNotes(timeNote.step,allnotes))
-
-    return trumpetvalues,pianovalues,allvalues
-
-
-
-unfiltered_values_gauss = dofft("gaussian")
-unfiltered_values_hanning = dofft("hanning")
-
-gaussvalues = filter(unfiltered_values_gauss,"gaussian")
-paravalues = filter(unfiltered_values_hanning,"parabolic")
-
-trumpetvalues, pianovalues, allvalues = pianovtrumpet(gaussvalues)
-
-printvalues(allvalues)
-printvalues(pianovalues)
+        if note.frequency < 2000 and note.frequency > 10 and note.amplitude > BASE_TRESH:
+            audiblefreqs.append(note)
+    return audiblefreqs
 
 def get_feq_amp(values,step):
     frq = []
@@ -200,6 +166,106 @@ def get_feq_amp(values,step):
         frq.append(note.frequency)
         amp.append(note.amplitude)
     return frq, amp
+
+def certainty(note):
+    notes = [16.35, 17.32, 18.35, 19.44, 20.6, 21.82, 23.12, 24.5, 25.95, 27.5, 29.13, 30.86, 32.7, 34.64, 36.7, 38.89, 41.2, 43.65, 46.24, 48.99, 51.91, 54.99, 58.26, 61.73, 65.4, 69.29, 73.41, 77.77, 82.4, 87.3, 92.49, 97.99, 103.82, 109.99, 116.53, 123.46, 130.8, 138.58, 146.82, 155.55, 164.8, 174.6, 184.98, 195.98, 207.63, 219.98, 233.06, 246.92, 261.6, 277.16, 293.64, 311.1, 329.6, 349.19, 369.96, 391.96, 415.26, 439.96, 466.12, 493.84, 523.2, 554.31, 587.27, 622.19, 659.19, 698.39, 739.92, 783.91, 830.52, 880.0, 932.24, 987.68, 1046.48, 1108.62, 1174.55, 1244.37, 1318.38, 1396.78, 1479.83, 1567.82, 1661.23, 1760.0, 1864.48, 1975.37, 2092.95, 2217.25, 2349.09, 2488.73, 2636.75, 2793.55, 2959.65, 3135.65, 3322.45, 3520.0, 3728.96, 3950.74, 4185.9, 4434.5, 4698.18, 4977.46, 5273.5, 5587.1, 5919.29, 6271.3, 6644.9, 7040.0, 7457.91, 7901.48, 8371.8, 8869.0, 9396.37, 9954.92, 10547.01, 11174.21, 11838.59, 12542.6, 13289.79, 14080.0, 14915.83, 15803.0, 16743.59, 17739.01, 18792.75, 19909.84]
+
+    i = 0
+    while i < len(notes) :
+        if notes[i] > note.frequency:
+            high_freq = notes[i]
+            low_freq = notes[i-1]
+            i = len(notes)   #stop while
+        i += 1
+    diff = min(abs(note.frequency-low_freq),abs(note.frequency-high_freq))
+
+    return diff/(high_freq-low_freq)
+
+
+def freq_anal(values):
+
+    return_values = []
+
+    for timeNote in values:
+        base_notes = getaudiblefreqs(timeNote.notes)
+        base_notes_ref = getaudiblefreqs(timeNote.notes)
+        if timeNote.step == 170:
+            for n in base_notes:
+                print(n)
+
+        result = []
+
+        for base_noteA in base_notes_ref:
+            for base_noteB in base_notes_ref:           ## remove harmonics from basenotes
+                fraction =  base_noteA.frequency/base_noteB.frequency
+                fraction_excess = (fraction+0.5) %1 -0.5
+                fraction_int = int(fraction-fraction_excess)
+                #if timeNote.step == 170:
+                #    print(base_noteA.frequency,base_noteB.frequency,fraction_excess)
+
+                if abs(fraction_excess) < 0.05 and fraction_int>1:
+                    if base_noteA in base_notes:
+                        base_notes.remove(base_noteA)
+
+        for base_note in base_notes:
+            bunch = []
+            #print(certainty(base_note),base_note.frequency)
+            for note in timeNote.notes:
+
+                fraction =  note.frequency/base_note.frequency
+                fraction_excess = (fraction+0.5) %1 -0.5
+                fraction_int = int(fraction-fraction_excess)
+
+                #if timeNote.step == 170:
+                #    print(fraction_int,base_note.frequency,note,fraction_excess)
+
+                if abs(fraction_excess) < 0.05:
+                    bunch.append([fraction_int,note])
+
+            result.append(bunch)
+
+        r = []
+
+        for bunch in result:
+            if len(bunch) >1:
+                r.append(bunch[0][1])
+
+
+
+        return_values.append(timeNotes(timeNote.step,r))
+
+    return return_values
+
+def hide_noise(values,strength):
+    outvalues = []
+    for i in range(strength,len(values)-strength):
+        enclosing_notes = []
+        for note in values[i-strength].notes:
+            enclosing_notes.append(note.name())
+        for note in values[i+strength].notes:
+            enclosing_notes.append(note.name())
+
+        outnotes = []
+
+        for note in values[i].notes:
+            name = note.name()
+            if name in enclosing_notes:
+                outnotes.append(note)
+        outvalues.append(timeNotes(values[i].step,outnotes))
+    return outvalues
+
+
+unfiltered_values_gauss = dofft("gaussian")
+#unfiltered_values_hanning = dofft("hanning")
+
+gaussvalues = filter(unfiltered_values_gauss,"gaussian")
+#paravalues = filter(unfiltered_values_hanning,"parabolic")
+
+allvalues = freq_anal(gaussvalues)
+
+allvalues = hide_noise(hide_noise(allvalues,2),1)
+
+printvalues(allvalues)
 
 
 
@@ -219,16 +285,16 @@ def submit(text):
     frq_unf_gauss, amp_unf_gauss = get_feq_amp(unfiltered_values_gauss,step)
     frq_fil_gauss, amp_fil_gauss = get_feq_amp(gaussvalues,step)
 
-    frq_unf_hanning, amp_unf_hanning = get_feq_amp(unfiltered_values_hanning,step)
-    frq_fil_para, amp_fil_para = get_feq_amp(paravalues,step)
+    #frq_unf_hanning, amp_unf_hanning = get_feq_amp(unfiltered_values_hanning,step)
+    #frq_fil_para, amp_fil_para = get_feq_amp(paravalues,step)
 
     # Create a figure containing a single Axes.
     ax.plot(frq_unf_gauss,amp_unf_gauss,label="gauss window",color="red")# Plot some data on the Axes.
     ax.scatter(frq_unf_gauss,amp_unf_gauss, s=2)
     ax.scatter(frq_fil_gauss,amp_fil_gauss,label = "gauss interpolation", color ="orange")
 
-    ax.plot(frq_unf_hanning,amp_unf_hanning,label="hanning window",color="blue")
-    ax.scatter(frq_fil_para,amp_fil_para,label = "parabolic interpolation", color="green")
+    #ax.plot(frq_unf_hanning,amp_unf_hanning,label="hanning window",color="blue")
+    #ax.scatter(frq_fil_para,amp_fil_para,label = "parabolic interpolation", color="green")
 
     ax.legend()
     ax.grid()
@@ -251,7 +317,27 @@ def plot():
     plt.show()
 
 fig, ax = plt.subplots()
+
+
+import WriteMidiFile
+
+def test_write_midi_file(values):
+    # Test the creation of a MIDI file and adding a piano instrument
+    file = WriteMidiFile.create_midi_file()
+    WriteMidiFile.add_piano(file)
+    WriteMidiFile.add_trumpet(file)
+
+    # Add notes to the MIDI file
+    for timeNote in values:
+        for note in timeNote.notes:
+
+            WriteMidiFile.add_note(file.instruments[0], note.name(), timeNote.second, FFT_WINDOW_SECONDS)
+
+    # Save the MIDI file
+    WriteMidiFile.write_midi_file(file, "media/midi/test_output.mid")
+    print("MIDI file created successfully.")
+
+
+test_write_midi_file(allvalues)
+
 plot()
-
-
-
