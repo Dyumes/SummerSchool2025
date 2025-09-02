0d530bfb2cfe8f8efb8cc81c02496bf4b5165681
louis272 <louis.schanen@gmail.com>
2025-09-02 10:21:18 +0200

    Merge

--- src/Constants.py
+++ src/Constants.py
@@ -1,9 +1,7 @@
 import math
 import pyautogui
 
-
-
-#Particles
+""" Particles """
 NBR_TRIANGLE_IN_CIRCLE = 5
 CIRCLE_RADIUS = 10/2560 * pyautogui.size()[0]
 SUN_PARTICLE_RADIUS = 5
@@ -19,14 +17,15 @@ GRAVITY_DIRECTION = math.pi / 2
 SUN_GRAVITY_MAGNITUDE = 1
 MARGIN_SUN_PARTICLES = 100
 
-#Generation
-min_depth = int(600/2560 * pyautogui.size()[0])
-max_depth = int(1100/2560 * pyautogui.size()[0])
-
-
-#Music process
+""" Music process """
+#midi_file = "SSB.mid"
+#mp3_file = "SSB.mp3"
 midi_file = "Ecossaise_Beethoven.midi"
 mp3_file = "Ecossaise_Both.mp3"
 
-#Palm
+""" Generation """
+min_depth = int(600/2560 * pyautogui.size()[0])
+max_depth = int(1100/2560 * pyautogui.size()[0])
+
+""" Palm """
 NB_PALM = 10
